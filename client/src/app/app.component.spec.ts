38643f8786470636524c2df889fc1cee521ca4e4
AnassEREKYSY <ereanass@gmail.com>
2025-02-28 14:05:58 +0100

    Style correction

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'client' title`, () => {
+  it(`should have the 'CoinHawk' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('CoinHawk');
   });
 
   it('should render title', () => {
